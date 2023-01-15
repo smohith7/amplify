c1525e8fbcdf52ee0b37a2ef3595b37f84c51702
mohith <smohi@gamil.com>
2023-01-15 14:00:25 +0530

    commit master

---
+++ goh.js
@@ -0,0 +1,5 @@
+var x = 11;
+if (x % 2 == 0)
+    console.log('even');
+else
+    console.log('odd');
---
+++ goh.ts
@@ -0,0 +1,6 @@
+
+var x = 11;
+if (x % 2 == 0)
+    console.log('even');
+else
+    console.log('odd');
